a643ad1bdc8c02aa9b53c6693ed18a5eadbabca5
Sedatkus <118366314+Sedatkus@users.noreply.github.com>
2022-11-17 11:36:23 +0300

    Add files via upload

---
+++ sartblockları123.py
@@ -0,0 +1,52 @@
+print("HOŞGELDİNİZ")
+#Ödev1 
+   #2 adet sayısal değişken tanımlayınız.
+    #Bu sayılara istediğiniz değeri atayınız.
+     #Bu sayılardan hangisi büyükse, ekrana yazdırınız.
+
+sayi1 = 8
+sayi2 = 6
+
+if sayi1 > sayi2 :
+    print(sayi1)
+elif sayi1 < sayi2 :
+    print(sayi2)
+else:
+    print("Eşittir")
+
+#Ödev2
+    #3 adet sayısal değişken tanımlayınız.
+    #Bu sayılardan en büyük ve en küçük olanı ekrana ayrı ayrı yazdırınız.
+
+sayi3 = 1
+sayi4 = 2
+sayi5 = 3
+
+if sayi3 > sayi4 and sayi3 > sayi5 :
+    print(sayi3)
+elif sayi4 > sayi3 and sayi4 > sayi5 :
+    print(sayi4)
+else : 
+    print(sayi5)
+
+if sayi3 < sayi4 and sayi3 < sayi5 :
+    print(sayi3)
+elif sayi4 < sayi3 and sayi4 < sayi5 :
+    print(sayi4)
+else :
+    print(sayi5)
+
+#ÖDEV 3 
+# altin.in web sayfasına gidiniz.
+# Burada şarta bağlı değişken kısımları bulunuz.
+#Şarta bağlı değişken kısımlar nerelerdir? 
+#1- Sayfanın ortasındaki 'durum' kısmındaki artış,azalış okları.
+#2- Sayfanın üst kısmında kurların üzerinde yanan yeşil kırmızı renk.Artışta Yeşil  , Azalışta Kırmızı 
+#3Kullanıcı Giriş ekranı 
+#3-TL/Gram altın çevirici
+#4-En iyi aktif 10 üye tablosu
+#5-Canlı grafikler
+
+
+
+
